0872082297974e7c36316c8fc54b88a44755e082
Proking73 HP <ishantgupta020@gmail.com>
2024-08-14 22:14:54 -0400

    Trying to fix overflow on mobile, added text-shadow on header and img for mobile but never actual image (link for file in head).

--- script.js
+++ script.js
@@ -2,5 +2,4 @@ const rexImgMain = document.querySelector(".rexImgMain");
 
 rexImgMain.addEventListener('animationend', () => {
     rexImgMain.style.animation = "rexImgMainWiggle 4s ease-in-out infinite";
-    console.log("hi");
 })
